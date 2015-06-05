943c744d66a383614a3cdb17eab5bf30daf8a08d
dengshengjin <dengshengjin0804@qq.com>
2015-06-05 18:00:38 +0800

    commit upgrade sessionId

--- ETicket_Validation_Terminal/app/src/main/java/com/ticket/validation/terminal/SysInfoActivity.java
+++ ETicket_Validation_Terminal/app/src/main/java/com/ticket/validation/terminal/SysInfoActivity.java
@@ -143,7 +143,11 @@ public class SysInfoActivity extends BaseUserActivity {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                AppDownloadManager.getInstance(getApplicationContext()).download(downloadUrl, AppUtil.getAppName(getApplicationContext()));
+                if (LoginInterceporUtil.pauseRedirect(getApplicationContext())) {
+                    return;
+                }
+                String realDownloadUrl = downloadUrl + "?sessionId=" + CacheDBUtil.getSessionId(getApplicationContext()) + "&outId=" + System.currentTimeMillis() + "&device=2";
+                AppDownloadManager.getInstance(getApplicationContext()).download(realDownloadUrl, AppUtil.getAppName(getApplicationContext()));
             }
         }).create().show();
     }
